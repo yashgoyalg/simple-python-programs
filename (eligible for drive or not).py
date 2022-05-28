0bd17b99b5bb7cdf0c04c6ee91ccd61bc0490c72
Yash G <98229201+yashgoyalg@users.noreply.github.com>
2022-05-29 00:35:27 +0530

    Add files via upload

---
+++ (eligible for drive or not).py
@@ -0,0 +1,11 @@
+#WAP in python to check whether the given age is eligible for drive or not?
+
+
+print("enter your ege!!")
+age = int(input())
+if age > 18:
+    print("You can drive")
+elif age==18:
+        print("SORRY, we can't decide to let you drive or not")
+else:
+            print("You can't drive")
